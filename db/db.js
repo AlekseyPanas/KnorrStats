50d3bf52a682cd5541c7d544a97975edad5595ff
AlekseyPanas <panasaleksey3@gmail.com>
2020-09-08 02:36:22 -0400

    Add Database Uploading (Buggy)

--- db/db.js
+++ db/db.js
@@ -25,6 +25,7 @@ const getClientPlayerData = async () => {
     return new_rows;
 };
 
+// Gets map names and ids for client
 const getClientMapData = async () => {
     // Saves rows of query
     let data = (await pool.query("SELECT map_id, map_name FROM maps")).rows;
@@ -38,10 +39,45 @@ const getClientMapData = async () => {
     }); 
     
     return new_rows;
+};
+
+// Uploads a game and returns the id
+const uploadGame = async (date, map_id, enemy_rounds, team_rounds) => {
+    let game_id = (await pool.query("insert into game (date, map_id, enemy_rounds, team_rounds) values ($1, $2, $3, $4) returning game_id", 
+                                    [date, map_id, enemy_rounds, team_rounds])).rows[0].game_id;
+
+    return game_id;
+}
+
+// Uploads player stats if the player is absent (all null)
+const uploadAbsentStats = async(player_id, game_id, is_absent) => {
+    await pool.query("insert into game_stats (player_id, game_id, isabsent) values ($1, $2, $3);", [player_id, game_id, is_absent]);
+};
+
+// Uploads player stats normally
+const uploadStats = async(player_id, game_id, kills, assists, score, mvps, adr, hs, ud, ef, ks3, ks4, ks5, score_pos, kill_pos, is_absent) => {
+    await pool.query('insert into game_stats (player_id, game_id, kills, assists, score, mvps, adr, hs, ud, ef, "3ks", "4ks", "5ks", score_pos, kill_pos, isabsent) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16);', 
+                     [player_id, game_id, kills, assists, score, mvps, adr, hs, ud, ef, ks3, ks4, ks5, score_pos, kill_pos, is_absent]);
+};
+
+// Uploads crit or comment
+const uploadCommend = async(player_id, date, is_commend, reason) => {
+    await pool.query("insert into commends (player_id, date, is_commend, reason) values ($1, $2, $3, $4);", [player_id, date, is_commend, reason]);
+};
+
+// Upload player daily
+const uploadDaily = async(date, id, total_pts, game_pts, kill_pts, score_pts, adr_pts, multi_pts, crit_pts, abs_pts, is_excused, has_notif, penalty) => {
+    await pool.query("insert into daily (date, player_id, total_points, game_points, kill_points, score_points, adr_points, multikill_points, crit_points, absence_points, is_excused, has_notified, absence_penalty) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+                     [date, id, total_pts, game_pts, kill_pts, score_pts, adr_pts, multi_pts, crit_pts, abs_pts, is_excused, has_notif, penalty])
 }
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
     getClientPlayerData: () => getClientPlayerData(),
-    getClientMapData: () => getClientMapData()
+    getClientMapData: () => getClientMapData(),
+    uploadGame: (date, map_id, enemy_rounds, team_rounds) => uploadGame(date, map_id, enemy_rounds, team_rounds),
+    uploadAbsentStats: (player_id, game_id, is_absent) => uploadAbsentStats(player_id, game_id, is_absent),
+    uploadStats: (player_id, game_id, kills, assists, score, mvps, adr, hs, ud, ef, ks3, ks4, ks5, score_pos, kill_pos, is_absent) => uploadStats(player_id, game_id, kills, assists, score, mvps, adr, hs, ud, ef, ks3, ks4, ks5, score_pos, kill_pos, is_absent),
+    uploadCommend: (player_id, date, is_commend, reason) => uploadCommend(player_id, date, is_commend, reason),
+    uploadDaily: (date, id, total_pts, game_pts, kill_pts, score_pts, adr_pts, multi_pts, crit_pts, abs_pts, is_excused, has_notif, penalty) => uploadDaily(date, id, total_pts, game_pts, kill_pts, score_pts, adr_pts, multi_pts, crit_pts, abs_pts, is_excused, has_notif, penalty)
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 const db = require('../db/db');
 const { response } = require('express');
+const { Pool } = require('pg');
 var router = express.Router();
+
+
+
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   res.render('index', { title: 'Foo' });
 });
+
+
+
 
 /* Ajax call used by pygame client to retrieve player data */
 router.get('/ajax/getplayers', async (req, res, next) => {
@@ -19,6 +26,9 @@ router.get('/ajax/getplayers', async (req, res, next) => {
   }
 });
 
+
+
+
 /* Ajax call used by pygame client to retrieve map data */
 router.get('/ajax/getmaps', async(req, res, next) => {
   let error = false;
@@ -30,15 +40,209 @@ router.get('/ajax/getmaps', async(req, res, next) => {
   }
 });
 
+
+
+router.get('/ajax/debug', (req, res, next) => {
+  let date = new Date('4/14/2003');
+  console.log(date.toDateString());
+
+  res.send("reply");
+})
+
+
+
 /* Ajax call used by pygame client to upload game data */
 router.post('/ajax/upload_data', async(req, res, next) => {
-  success = true;
+  // Variable to track any connection errors
+  let success = true;
 
-  console.log(req.body)
+  // Gets posted json object with all the data
+  let uploadJSON = req.body;
+
+  // Date for this upload (grabbed from first game)
+  let date = new Date(uploadJSON.games[0].date);
+
+  // Creates an object used for calculating points for each player
+  let player_ids = (await db.query("select player_id from player")).rows;
+  let points_json = [];
+  for (i of player_ids) {
+    points_json.push({
+      "player_id": i.player_id,
+      "game_pts": 0,
+      "kill_pts": 0,
+      "score_pts": 0,
+      "adr_pts": 0,
+      "multikill_pts": 0,
+      "crit_pts": 0,
+      "absence_pts": 0
+    });
+  };
+
+  // Iterates through game data
+  for (game of uploadJSON.games) {
+    // game stats
+    let map_id = game.map_id;
+    let team_rounds = game.team_rounds;
+    let enemy_rounds = game.enemy_rounds;
+
+    // Determines points for game outcome
+    if (enemy_rounds > team_rounds) {
+      game_points = -3;
+    } else if (team_rounds > enemy_rounds) {
+      game_points = 7;
+    } else {
+      game_points = 2;
+    };
+
+    // Gives points for game outcome
+    for (player of points_json) {
+      player.game_pts += game_points;
+    };
+
+    // Uploads game and retrieves game_id
+    // >>>>>>>>>>>>>> let gameID = await db.uploadGame(date, map_id, enemy_rounds, team_rounds);
+
+    // Iterates through player data
+    for (player of game.player_data) {
+      // Uploads players stats (if absent, null values)
+      if (player.isabsent) {
+        // >>>>>>>>>>>>> await db.uploadAbsentStats(player.player_id, gameID, player.isabsent);
+      } else {
+        /* >>>>>>>>>>> await db.uploadStats(player.player_id, gameID, player.kills, player.assists,
+                             player.score, player.mvps, player.adr, player.hs, player.ud, player.ef,
+                             player["3ks"], player["4ks"], player["5ks"], player.score_pos, player.kill_pos, player.isabsent);*/
+      };
+
+      // Adds points
+      target_points = points_json.filter(ply => ply.player_id == player.player_id)[0]
+      
+      let kill_points;
+      switch (player.kill_pos) {
+        case 1:
+          kill_points = 5;
+        case 2:
+          kill_points = 3;
+        case 3:
+          kill_points = 1;
+        case 4:
+          kill_points = -1;
+        case 5:
+          kill_points = -2;
+      }
+
+      let score_points;
+      switch (player.score_pos) {
+        case 1:
+          score_points = 5;
+        case 2:
+          score_points = 3;
+        case 3:
+          score_points = 1;
+        case 4:
+          score_points = -1;
+        case 5:
+          score_points = -2;
+      }
+      
+      if (!player.isabsent) {
+        target_points.kill_pts += kill_points;
+        target_points.score_pts += score_points;
+        target_points.multikill_pts += (player["3ks"]) + (player["4ks"] * 3) + (player["5ks"] * 5);
+        target_points.adr_pts += Math.round((0.05 * player.adr) - 2);
+      };
+    };
+  };
+
+  // Iterates through player daily data
+  for (player of uploadJSON.player_daily) {
+    let commend_points;
+    let crit_points;
+
+    // Uploads commend and/or criticism for player and calculates points
+    if (player.is_commend) {
+      // >>>>>>>>>>> db.uploadCommend(player.player_id, date, true, player.commend_reason);
+
+      commend_points = (await db.query("select * from commends where player_id = $1 and extract(month from date) = $2 and is_commend = TRUE;", [player.player_id, date.getMonth()])).rows.length * 5
+      if (commend_points > 30) {
+        commend_points = 30;
+      };
+
+    };
+    if (player.is_criticism) {
+      // >>>>>>>>>>>>> db.uploadCommend(player.player_id, date, false, player.criticism_reason);
+
+      crit_points = (await db.query("select * from commends where player_id = $1 and extract(month from date) = $2 and is_commend = FALSE;", [player.player_id, date.getMonth()])).rows.length * 5
+      if (crit_points > 30) {
+        crit_points = 30;
+      };
+
+    };
+
+    // Sets crit/commend points
+    target_points = points_json.filter(ply => ply.player_id == player.player_id)[0];
+    target_points.crit_pts += commend_points;
+    target_points.crit_pts -= crit_points;
+    
+    // Absence penalties
+    let absence_points = 0;
+
+    let penalty = 0;
+    let is_main_roster = (await db.getClientPlayerData()).filter(row => row.player_id == player.player_id)[0].ismainroster;
+
+    let game_quant = uploadJSON.games.length;
+    let num_full_queue = uploadJSON.games.filter(game => game.player_data.filter(pl => !pl.isabsent).length == 5).length;
+    let num_partial_queue = game_quant - num_full_queue
+
+    let games_participated = uploadJSON.games.filter(game => !game.player_data.filter(pl => pl.player_id == player.player_id)[0].isabsent).length;
+
+    // Checks main roster penalty conditions (Gone for whole night, no excuse, penalty for games that werent 5q)
+    if (is_main_roster) {
+      if (!player.is_excused && games_participated == 0) {
+        penalty += num_partial_queue;
+      };
+    } 
+    // Checks non main roster penalty conditions (Gone for whole night, not a single 5q was player, one penalty for whole night)
+    else {
+      if (!player.is_excused && games_participated == 0 && num_full_queue == 0) {
+        penalty += 1;
+      }
+    };
+
+    // Counts penalties for this month previously
+    let penalized_absences = (await db.query("select absence_penalty from daily where player_id = $1 and extract(month from date) = $2", [player.player_id, date.getMonth()])).rows
+    let penalized_absence_count = 0;
+    for (row of penalized_absences) {
+      penalized_absence_count += row.absence_penalty;
+    }
+    // Adds new penalties and calculates point deduction
+    for (i = 0; i < penalty; i++) {
+      penalized_absence_count += i + 1
+      absence_points -= penalized_absence_count
+    }
+
+    // Calculates notification deduction
+    if (is_main_roster && !player.has_notified) {
+      absence_points -= (await db.query("select has_notified from daily where player_id = $1 and extract(month from date) = $2;", [player.player_id, date.getMonth()])) + 1
+    }
+
+    // The actual deduction of absence points
+    target_points.absence_pts -= absence_points;
+
+    let total_points = target_points.score_pts + target_points.game_pts + target_points.kill_pts + target_points.absence_pts + target_points.crit_pts + target_points.adr_pts + target_points.multikill_pts;
+    console.log(target_points)
+    // Uploads to database
+    await db.uploadDaily(date, player.player_id, total_points, target_points.game_pts, target_points.kill_pts,
+      target_points.score_pts, target_points.adr_pts, target_points.multikill_pts, target_points.crit_pts, target_points.absence_pts, 
+      player.is_excused, player.has_notified, penalty);
+  };
+
+  
+
   await db.getClientMapData().catch(async err => {
-    success = false; return err
+    success = false; return err;
   });
 
+  // Responds with upload success (false if error)
   res.json({success: success});
 })
 
